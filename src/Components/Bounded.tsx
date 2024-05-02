6c4454e4e5822ad097fc23bb11cac961fd9aae44
Akshey-Gupta <subashakshey@gmail.com>
2024-05-03 02:34:41 +0530

    chnaged

--- src/Components/Bounded.tsx
+++ src/Components/Bounded.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import clsx from 'clsx'
+import React from "react";
+import clsx from "clsx";
 
 type BoundedProps = {
-    as?: React.ElementType,
-    className?: string,
-    children?: React.ReactNode,
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
 };
 
-const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(({
-    as: Comp = "section", className, children, ...restProps }, ref) => {
-        return (
-            <Comp ref={ref}
-                className={clsx("px-10 py-10 md:px-10 md:py-14 lg:py-16", className)}
-                {...restProps}>
-                <div className='mx-auto w-full max-w-7x1'>{children}</div>
-            </Comp>
-        )
-    })
-Bounded.displayName = 'Bounded';
+const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
+  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
+      </Comp>
+    );
+  },
+);
 
-export default Bounded
+// Set a display name for the component
+Bounded.displayName = "Bounded";
+
+export default Bounded;
