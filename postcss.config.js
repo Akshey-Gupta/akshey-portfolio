4e69509aa543efd27668dbc525b00d269b41e328
Akshey-Gupta <subashakshey@gmail.com>
2024-05-03 07:21:07 +0530

    change

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// next.config.js
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  purgeCSS: {
-    whitelist: ['px-4', 'py-10', 'md:px-6', 'md:py-14', 'lg:py-16'], // Add any Tailwind CSS classes you want to whitelist
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
